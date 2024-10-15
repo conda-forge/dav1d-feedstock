c4c6d5a51fbf46f835a82d8797051a42e57df397
jaimergp <jaimergp@users.noreply.github.com>
2024-10-16 01:23:08 +0200

    debug

--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -80,8 +80,7 @@ call :end_group
 
 :: Build the recipe
 echo Building recipe
-set "CONDA_VERBOSITY=3"
-conda-build.exe "recipe" -m .ci_support\%CONFIG%.yaml --suppress-variables %EXTRA_CB_OPTIONS%
+conda-build.exe --debug "recipe" -m .ci_support\%CONFIG%.yaml --suppress-variables %EXTRA_CB_OPTIONS%
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 call :start_group "Inspecting artifacts"
