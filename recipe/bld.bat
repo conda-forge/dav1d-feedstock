928d5b2b15da6651715122a2dcd50b71309ce583
Daniel Ching <carterbox@users.noreply.github.com>
2024-10-20 12:51:01 -0500

    REF: Remove custom install strip and prefix patching

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,7 @@
 @echo on
 
-mkdir %SRC_DIR%\stage
-if errorlevel 1 exit 1
-
 meson setup builddir           ^
     %MESON_ARGS%               ^
-    --prefix=%SRC_DIR%\stage   ^
     -Denable_tests=false
 if errorlevel 1 exit 1
 
@@ -14,12 +10,3 @@ if errorlevel 1 exit 1
 
 meson install -C builddir --no-rebuild
 if errorlevel 1 exit 1
-
-setlocal EnableExtensions ENABLEDELAYEDEXPANSION
-
-for %%f in ( "%SRC_DIR%\stage\lib\pkgconfig\*.pc" ) do (
-    sed -i.bak "s,prefix=.*,prefix=/opt/anaconda1anaconda2anaconda3/Library,g" %%f
-    del %%f.bak
-)
-
-endlocal
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,9 @@
 set -ex
 
-mkdir $SRC_DIR/stage
-
 meson setup builddir          \
     ${MESON_ARGS}             \
-    --prefix=$SRC_DIR/stage   \
     -Denable_tests=false
 
 meson compile -C builddir
 
 meson install -C builddir --no-rebuild
-
-sed -i.bak "s,$SRC_DIR/stage,/opt/anaconda1anaconda2anaconda3,g" $SRC_DIR/stage/lib/pkgconfig/*.pc
-rm $SRC_DIR/stage/lib/pkgconfig/*.bak
