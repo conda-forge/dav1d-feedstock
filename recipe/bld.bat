89c146a5473a5e7a8aedc1a34bfef243eff1e1b3
Daniel Ching <9604511+carterbox@users.noreply.github.com>
2024-10-22 13:07:53 -0500

    MSVC does not support --strip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,5 +8,5 @@ if errorlevel 1 exit 1
 meson compile -C builddir
 if errorlevel 1 exit 1
 
-meson install -C builddir --no-rebuild --strip
+meson install -C builddir --no-rebuild
 if errorlevel 1 exit 1
